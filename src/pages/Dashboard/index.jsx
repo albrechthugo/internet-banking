f1e0e4b17842737c32be7df8f39317da12c35716
Hugo <albrechthugo4@gmail.com>
2020-10-17 22:21:00 -0300

    dashboard internetbanking criado - renderizando barra lateral

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+import LeftSide from '../../components/LeftSide';
+
+const Dashboard = () => {
+  return (
+    <div id="dashboard__container">
+      <LeftSide />
+    </div>
+  );
+}
+
+export default Dashboard;
