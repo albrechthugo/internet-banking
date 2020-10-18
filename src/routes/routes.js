60e6bd6d524da1dbed342fef4d9668a9cf1ca78f
Hugo <albrechthugo4@gmail.com>
2020-10-17 22:15:40 -0300

    nova rota adicionada

---
+++ src/routes/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
