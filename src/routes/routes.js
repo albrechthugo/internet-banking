5ff1d993372a1b519223a4662ccf290732c12763
Hugo <albrechthugo4@gmail.com>
2020-10-18 23:03:38 -0300

    novas rotas adicionadas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
+import Cards from '../pages/Cards';
+import Extract from '../pages/Extract';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="/dashboard/cards" component={Cards} />
+        <Route path="/dashboard/details" component={Extract} />
       </Switch>
     </BrowserRouter>
   );
