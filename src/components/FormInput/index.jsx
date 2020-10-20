ba2b61cfd0eae0fa2ad4a09bd2ecfee63f569bae
Hugo <albrechthugo4@gmail.com>
2020-10-20 19:12:30 -0300

    formulario armazenando id para consulta da empresa

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles.css';
 
 const FormInput = (props) => {
+  const [empresa, setEmpresa] = useState('');
+
+  const getInputText = (event) => {
+    setEmpresa(event.target.value);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('id', empresa)
+  }, [empresa]);
+
   return (
     <div className="login__form email">
       <label>
         {props.title}
       </label>
-        <input type="text"/>
+        <input type="text" onChange={getInputText} value={empresa}/>
     </div>
   );
 }
